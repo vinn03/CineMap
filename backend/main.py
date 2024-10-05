714acbbc0bd283d57e72b1988c81c0755cde0900
kevingky01 <kevinguo2019@gmail.com>
2024-10-05 14:56:40 -0700

    added get for users and films

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -12,9 +12,18 @@ with open("./models/films.json", "r") as f:
 @app.route("/user", methods=["GET"])
 #find the name of the user with the given email
 def get_user():
-    return users
+    #called_email = request.args.get("email")
+    called_email = "janedoe@gmail.com"
+    for user in users:
+        if user["email"] == called_email:
+            user_return_value = {
+                "email": user["email"],
+                "display_name": user["display_name"],
+                "films": user["films"]
+            }
+            return json.dumps(user_return_value)
 
 @app.route("/films", methods=["GET"])
 #return films
 def get_films():
-    return "<p>Map</p>"
+    return json.dumps(films)
