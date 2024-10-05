7f5a74b02c325ad7d76d4ea8547f73e8bca30925
Vince <112585869+vinn03@users.noreply.github.com>
2024-10-05 14:12:26 -0700

    added backend code to extract json

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+
+with open("users.json", "r") as u:
+    users = json.load(u)
+
+with open("films.json", "r") as f:
+    films = json.load(f)
 
 @app.route("/")
 def hello_world():
