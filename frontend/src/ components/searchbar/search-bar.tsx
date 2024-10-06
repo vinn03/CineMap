5c7ad6eadef21a9d35122c554f253e00d25e0475
Vince <112585869+vinn03@users.noreply.github.com>
2024-10-05 23:03:43 -0700

    search complete

--- frontend/src/ components/searchbar/search-bar.tsx
+++ frontend/src/ components/searchbar/search-bar.tsx
@@ -1,38 +1,66 @@
-import ReactSearchBox from "react-search-box";
+import { useState } from "react";
+import Films from "../../../models/films.json";
+import Users from "../../../models/users.json";
+import "../../index.css";
 
 function SearchBar() {
-  const data = [
-    {
-      key: "john",
-      value: "John Doe",
-    },
-    {
-      key: "jane",
-      value: "Jane Doe",
-    },
-    {
-      key: "mary",
-      value: "Mary Phillips",
-    },
-    {
-      key: "robert",
-      value: "Robert",
-    },
-    {
-      key: "karius",
-      value: "Karius",
-    },
-  ];
+  const [query, setQuery] = useState("");
+  const [category, setCategory] = useState("films");
+
+  const filteredFilms = Films.filter((item) => {
+    return item.title.toLowerCase().includes(query.toLowerCase());
+  });
+
+  const filteredUsers = Users.filter((item) => {
+    return item.display_name.toLowerCase().includes(query.toLowerCase());
+  });
+
+  const handleKeyPress = (e) => {
+    const handleSearch = (query) => {
+      if (!query) return;
+
+      console.log("Searched for:", query);
+    };
+
+    if (e.key === "Enter") {
+      handleSearch(query);
+    }
+  };
 
   return (
-    <>
-      <ReactSearchBox
-        placeholder="Dummy Text"
-        value="Doe"
-        data={data}
-        callback={() => console.log("triggered")}
-      />
-    </>
+    <div>
+      <div>
+        <input
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleKeyPress}
+        />
+        <span>
+          {" "}
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="films">Films</option>
+            <option value="users">Users</option>
+          </select>
+        </span>
+      </div>
+      <div>
+        {category === "films"
+          ? filteredFilms.map((d) => (
+              <button className="entry" key={d.id}>
+                {d.title}
+              </button>
+            ))
+          : filteredUsers.map((d) => (
+              <button className="entry" key={d.email}>
+                {d.display_name}
+              </button>
+            ))}
+      </div>
+    </div>
   );
 }
 
--- frontend/src/ components/sections/saved-films.tsx
+++ frontend/src/ components/sections/saved-films.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./sections.css";
+import "../../index.css";
 
 interface SavedFilmsProps {
   films: string[];
@@ -9,7 +10,7 @@ const SavedFilms: React.FC<SavedFilmsProps> = ({ films }) => {
   return (
     <div className="section">
       {films.map((film, index) => (
-        <button key={index} className="film-entry">
+        <button key={index} className="entry">
           {film}
         </button>
       ))}
