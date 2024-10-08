2e2662ef8037eedaa55fc51f869af9dbbb0e71f7
Leo Zhi <leozhi6869@gmail.com>
2024-10-07 20:45:42 -0700

    update UI and add more data to JSONs

--- frontend/src/ components/sections/overviews/film-overview.tsx
+++ frontend/src/ components/sections/overviews/film-overview.tsx
@@ -2,39 +2,41 @@ import React, { useEffect } from "react";
 import "./overview.css";
 
 interface FilmOverviewProps {
-  title: string;
-  year: string;
-  genre: string;
-  locations: any[];
-  setLocations: (locations: any[]) => void;
+	title: string;
+	year: string;
+	genre: string;
+	locations: any[];
+	setLocations: (locations: any[]) => void;
 }
 
 const FilmOverview: React.FC<FilmOverviewProps> = ({
-  title,
-  year,
-  genre,
-  locations,
-  setLocations,
+	title,
+	year,
+	genre,
+	locations,
+	setLocations,
 }) => {
-  useEffect(() => {
-    setLocations(locations);
-  }, [locations]);
+	useEffect(() => {
+		setLocations(locations);
+	}, [locations]);
 
-  return (
-    <div className="overview">
-      <button className="close" onClick={() => window.location.reload()}>
-        Close
-      </button>
-      <h1>{title}</h1>
-      <p>Release Date: {year}</p>
-      <p>Genre: {genre}</p>
-      <br></br>
-      <h2>Locations: </h2>
-      {locations.map((location, index) => (
-        <p key={index}>{location.title}</p>
-      ))}
-    </div>
-  );
+	return (
+		<div className="overview">
+			<button className="close" onClick={() => window.location.reload()}>
+				Close
+			</button>
+			<h1>{title}</h1>
+			<p>Release Date: {year}</p>
+			<p>Genre: {genre}</p>
+			<br></br>
+			<h2>Locations: </h2>
+			{locations.map((location, index) => (
+				<button key={index} className="location-button">
+					{location.title}
+				</button>
+			))}
+		</div>
+	);
 };
 
 export default FilmOverview;
