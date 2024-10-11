c4f2363691245fddbb53959d894a095429caef65
Vince <112585869+vinn03@users.noreply.github.com>
2024-10-11 10:57:01 -0700

    changed map styling

--- frontend/src/ components/map/map.tsx
+++ frontend/src/ components/map/map.tsx
@@ -100,7 +100,7 @@ const Map: React.FC<MapProps> = ({ locations, user }) => {
         ref={setMap}
       >
         <leaf.TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=JHp8pdRRwvaUIipR78yo"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <CustomZoomControl position="bottomright" />
